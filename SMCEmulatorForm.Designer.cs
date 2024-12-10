cea1e657579a1c8b6dd4cabe4988ef836958c6e7
Eli Wilson <ewilson@vacuumtechnology.com>
2024-12-10 12:25:51 -0500

    Add project files.

---
+++ SMCEmulatorForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace SMCEmulator
+{
+	partial class SMCEmulatorForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if(disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.COMPortDropdown = new System.Windows.Forms.ComboBox();
+			this.connectButton = new System.Windows.Forms.Button();
+			this.disconnectButton = new System.Windows.Forms.Button();
+			this.countLabel = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// COMPortDropdown
+			// 
+			this.COMPortDropdown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.COMPortDropdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.COMPortDropdown.FormattingEnabled = true;
+			this.COMPortDropdown.Location = new System.Drawing.Point(314, 235);
+			this.COMPortDropdown.Name = "COMPortDropdown";
+			this.COMPortDropdown.Size = new System.Drawing.Size(144, 40);
+			this.COMPortDropdown.TabIndex = 0;
+			// 
+			// connectButton
+			// 
+			this.connectButton.Location = new System.Drawing.Point(106, 310);
+			this.connectButton.Name = "connectButton";
+			this.connectButton.Size = new System.Drawing.Size(150, 68);
+			this.connectButton.TabIndex = 1;
+			this.connectButton.Text = "Connect";
+			this.connectButton.UseVisualStyleBackColor = true;
+			this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+			// 
+			// disconnectButton
+			// 
+			this.disconnectButton.Location = new System.Drawing.Point(513, 310);
+			this.disconnectButton.Name = "disconnectButton";
+			this.disconnectButton.Size = new System.Drawing.Size(150, 68);
+			this.disconnectButton.TabIndex = 2;
+			this.disconnectButton.Text = "Disconnect";
+			this.disconnectButton.UseVisualStyleBackColor = true;
+			this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+			// 
+			// countLabel
+			// 
+			this.countLabel.AutoSize = true;
+			this.countLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.countLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+			this.countLabel.Location = new System.Drawing.Point(307, 133);
+			this.countLabel.Name = "countLabel";
+			this.countLabel.Size = new System.Drawing.Size(161, 40);
+			this.countLabel.TabIndex = 3;
+			this.countLabel.Text = "0 Counts";
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// SMCEmulatorForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.countLabel);
+			this.Controls.Add(this.disconnectButton);
+			this.Controls.Add(this.connectButton);
+			this.Controls.Add(this.COMPortDropdown);
+			this.Name = "SMCEmulatorForm";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.SMCEmulatorForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox COMPortDropdown;
+		private System.Windows.Forms.Button connectButton;
+		private System.Windows.Forms.Button disconnectButton;
+		private System.Windows.Forms.Label countLabel;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ SMCEmulatorForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMCEmulator
+{
+	public partial class SMCEmulatorForm : Form
+	{
+		SerialPort port;
+		System.Threading.Thread comThread;
+		bool disconnectClicked = false;
+		int counts = 0;
+
+		public SMCEmulatorForm()
+		{
+			InitializeComponent();
+		}
+
+		private void populateComPortMenu()
+		{
+			COMPortDropdown.Items.AddRange(
+					System.IO.Ports.SerialPort.GetPortNames()
+						.OrderBy(s => s)
+						.Distinct()
+						.ToArray());
+		}
+
+		private void SMCEmulatorForm_Load(object sender, EventArgs e)
+		{
+			populateComPortMenu();
+			timer1.Enabled = true;
+		}
+
+		private string GetDropdownValue()
+		{
+			if(COMPortDropdown.SelectedItem != null)
+				return COMPortDropdown.SelectedItem.ToString();
+			else
+				return "";
+		}
+
+		private void SetButtonStates(bool connected)
+		{
+			if(connected)
+			{
+				connectButton.Enabled = false;
+				disconnectButton.Enabled = true;
+				connectButton.BackColor = Color.LightGreen;
+				disconnectButton.BackColor = Color.White;
+			}
+			else
+			{
+				connectButton.Enabled = true;
+				disconnectButton.Enabled = false;
+				disconnectButton.BackColor = Color.Red;
+				connectButton.BackColor = Color.White;
+			}
+
+		}
+
+		private void communicate()
+		{
+			string portName = (string)COMPortDropdown.Invoke(new Func<string>(() => GetDropdownValue()));
+			if(portName == "")
+			{
+				connectButton.Invoke(new Action(() => SetButtonStates(false)));
+				return;
+			}
+			port = new SerialPort(portName, 9600);
+			port.Open();
+			byte[] buffer;
+			string recd, msg;
+			Regex setRegex = new Regex("(SET )(\\d+)");
+			Match match;
+			while(!disconnectClicked)
+			{
+				int count = port.BytesToRead;
+				if(count >= 2)
+				{
+					buffer = new byte[count];
+					var str = port.Read(buffer, 0, count);
+					recd = Encoding.Default.GetString(buffer);
+					Console.WriteLine(recd);
+					match = setRegex.Match(recd);
+					if(match.Success)
+					{
+						Int32.TryParse(match.Groups[2].Value, out counts);
+						msg = "asdf";
+					}
+					else
+					{
+						msg = "error";
+					}
+					port.WriteLine(msg);
+					Console.WriteLine(msg);
+				}
+				else
+				{
+					Thread.Sleep(100);
+				}
+			}
+			disconnectClicked = false;
+			port.Close();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			countLabel.Text = counts.ToString() + " counts";
+		}
+
+		private void connectButton_Click(object sender, EventArgs e)
+		{
+			SetButtonStates(true);
+
+			comThread = new Thread(() => communicate());
+			comThread.Start();
+		}
+
+		private void disconnectButton_Click(object sender, EventArgs e)
+		{
+			SetButtonStates(false);
+			disconnectClicked = true;
+		}
+	}
+}
